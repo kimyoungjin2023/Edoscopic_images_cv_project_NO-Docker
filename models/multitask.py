d22868e96a6b86f1b4fcf96bbc26f49edd3e6c07
ysop <sysysy3142@gmail.com>
2026-01-30 11:21:11 +0900

    Mask R-CNN 기반 멀티태스크 모델 구조

--- models/multitask.py
+++ models/multitask.py
@@ -1,15 +1,23 @@
 # models/multitask.py
-import torch.nn as nn
-from models.unet import UNet
-from models.fast_rcnn import FastRCNN
+import torch
+from torchvision.models.detection import maskrcnn_resnet50_fpn
+from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
+from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
-class MultiTaskModel(nn.Module):
-    def __init__(self, num_classes=2):
-        super(MultiTaskModel, self).__init__()
-        self.segmentation = UNet(in_channels=3, out_channels=1)
-        self.detection = FastRCNN(num_classes=num_classes)
+class MultiTaskMaskRCNN(torch.nn.Module):
+    def __init__(self, num_classes):
+        super(MultiTaskMaskRCNN, self).__init__()
+        # Pre-trained Mask R-CNN 로드
+        self.model = maskrcnn_resnet50_fpn(weights="DEFAULT")
 
-    def forward(self, images, rois):
-        seg_out = self.segmentation(images)
-        cls_logits, bbox_preds = self.detection(images, rois)
-        return seg_out, cls_logits, bbox_preds
+        # [Detection] Head 수정: 클래스 수에 맞춤
+        in_features = self.model.roi_heads.box_predictor.cls_score.in_features
+        self.model.roi_heads.box_predictor = FastRCNNPredictor(in_features, num_classes)
+
+        # [Segmentation] Head 수정: 클래스 수에 맞춤
+        in_features_mask = self.model.roi_heads.mask_predictor.conv5_mask.in_channels
+        self.model.roi_heads.mask_predictor = MaskRCNNPredictor(in_features_mask, 256, num_classes)
+
+    def forward(self, images, targets=None):
+        # 학습 시에는 loss를, 추론 시에는 결과값을 반환함
+        return self.model(images, targets)
